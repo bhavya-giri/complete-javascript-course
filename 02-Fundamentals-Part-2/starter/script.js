0400b0f406548a3eafd757535c102705ae2c8cdf
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-01-02 17:18:16 +0530

    function excercies

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,18 @@
 "use strict";
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Dolphines win by(${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`Koalas win by (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log("no one wins");
+  }
+}
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+const avgDolphins2 = calcAverage(85, 54, 41);
+const avgKoalas2 = calcAverage(23, 34, 27);
+
+checkWinner(avgDolphins1, avgKoalas1);
+checkWinner(avgDolphins2, avgKoalas2);
