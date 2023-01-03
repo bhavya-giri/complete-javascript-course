1f9140e0f533a32b61d1485c37dc23f9313adc20
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-01-03 16:36:47 +0530

    array

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,18 +1,30 @@
-"use strict";
-const calcAverage = (a, b, c) => (a + b + c) / 3;
-function checkWinner(avgDolphins, avgKoalas) {
-  if (avgDolphins >= avgKoalas * 2) {
-    console.log(`Dolphines win by(${avgDolphins} vs ${avgKoalas})`);
-  } else if (avgKoalas >= avgDolphins * 2) {
-    console.log(`Koalas win by (${avgKoalas} vs ${avgDolphins})`);
-  } else {
-    console.log("no one wins");
-  }
-}
-const avgDolphins1 = calcAverage(44, 23, 71);
-const avgKoalas1 = calcAverage(65, 54, 49);
-const avgDolphins2 = calcAverage(85, 54, 41);
-const avgKoalas2 = calcAverage(23, 34, 27);
+// "use strict";
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// function checkWinner(avgDolphins, avgKoalas) {
+//   if (avgDolphins >= avgKoalas * 2) {
+//     console.log(`Dolphines win by(${avgDolphins} vs ${avgKoalas})`);
+//   } else if (avgKoalas >= avgDolphins * 2) {
+//     console.log(`Koalas win by (${avgKoalas} vs ${avgDolphins})`);
+//   } else {
+//     console.log("no one wins");
+//   }
+// }
+// const avgDolphins1 = calcAverage(44, 23, 71);
+// const avgKoalas1 = calcAverage(65, 54, 49);
+// const avgDolphins2 = calcAverage(85, 54, 41);
+// const avgKoalas2 = calcAverage(23, 34, 27);
 
-checkWinner(avgDolphins1, avgKoalas1);
-checkWinner(avgDolphins2, avgKoalas2);
+// checkWinner(avgDolphins1, avgKoalas1);
+// checkWinner(avgDolphins2, avgKoalas2);
+const calTips = (bill) => {
+  if (bill > 50 && bill < 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+const bills = [125, 555, 44];
+const tips = [calTips(bills[0]), calTips(bills[1]), calTips(bills[2])];
+console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
